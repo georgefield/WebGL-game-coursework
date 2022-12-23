12024ab6004215b08007269c3c55cc5180c0f657
George Field <george.field121@gmail.com>
2022-12-23 13:42:53 +0000

    colour attrib

--- game.js
+++ game.js
@@ -4,11 +4,10 @@ var points;
 function main() {
     var canvas = document.getElementById("gl-canvas");
 
-    gl = WebGLUtils.setupWebGL(canvas);
+    gl = WebGLUtils.setupWebGL(canvas); //what webgl-utils is needed for
     if (!gl) {
         alert("WebGL isn't available");
     }
-
 
     var vertices = new Float32Array(
 	[-0.5, -0.5,
@@ -16,10 +15,16 @@ function main() {
 	0.5, 0.5,
 	]);
 
+    var vColor = new Float32Array(
+    [1.0, 0.0, 1.0,
+    0.0, 1.0, 1.0,
+    1.0,1.0,0
+    ]);
+
     //  Configure WebGL
 
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearColor(0.3, 0.3, 0.5, 1.0);
     //  Load shaders and initialize attribute buffers
 
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
@@ -28,15 +33,26 @@ function main() {
 
     // Load the data into the GPU
 
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    //--position data
+    var vPosBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vPosBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
     // Associate our shader variables with our data buffer
-
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
+
+
+    //--color data
+    var vColBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vColBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vColor, gl.STATIC_DRAW);
+
+    //color attrib location
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
 
     render();
 };
